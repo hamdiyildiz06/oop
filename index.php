4590a4f60e3e3eaebb17a595fd168fb8acc9bcb7
hamdi Yıldız <hamdiyildiz06@gmail.com>
2021-06-27 16:08:35 +0300

    override kullanımı bitit 7

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
-class insan{
+class Varlik{
     public $boy;
     public $kilo;
     public $yas;
     public $isim    = "hamdi";
+    public $hayvan_turu = "köpek";
 
 
-    function __construct()
+    function __construct($isim)
     {
-
+        echo "selamlar ". $isim . "<br>";
     }
 
     public function kosmak(){
@@ -22,10 +23,31 @@ class insan{
     public function uyumak(){
         echo "insan uyumak<br>";
     }
+
+    function __destruct(){
+        echo "Güle güle ";
+    }
 }
 
-$mehmet = new insan();
+class Hayvanlar extends Varlik{
 
+    public function yemekyeme(){
+        echo "Hayvanlar yemek yer <br>";
+    }
+    public function kosmak()
+    {
+        echo $this->hayvan_turu . 'ler hızlı koşar <br>';
+    }
+}
+
+
+
+
+$mehmet = new Hayvanlar("hamdi");
+
+$mehmet->yemekyeme();
 $mehmet->kosmak();
-$mehmet->yurumek();
-$mehmet->uyumak();
+
+//$mehmet->kosmak();
+//$mehmet->yurumek();
+//$mehmet->uyumak();
